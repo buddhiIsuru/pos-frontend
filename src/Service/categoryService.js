2be5d8c5635aef6ea9b2354c468f5c75b5dc6ea8
BuddhiCommercialTP <buddhi@commercialtp.com>
2023-05-19 01:15:45 +0530

    change base url

--- src/Service/categoryService.js
+++ src/Service/categoryService.js
@@ -3,7 +3,7 @@ import { baseUrl } from '../constance/baseUrl';
 import { httpGET } from './intercepter';
 
 export const getCategoryOutletVice = (id) => {
-    const url = baseUrl + "/category/get-all-category";
-    // const url = baseUrl + "/category/get-category-by-outlet/"+id;
+    // const url = baseUrl + "/category/get-all-category";
+    const url = baseUrl + "/category/get-category-by-outlet/"+id;
     return httpGET(url);
 };
